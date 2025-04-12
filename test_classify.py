36c0a618e1c19e08e5a9db049b6909edfa472232
vento277 <blacksoup0112@gmail.com>
2025-04-11 21:28:33 -0700

    update

--- test_classify.py
+++ test_classify.py
@@ -1,73 +1,50 @@
-import csv
-import os
-from torchvision import datasets, transforms
+'''
+This code is used to generate the final submission csv file and .npy logits output of the test set.
+Most of this file iscopied over from classification_evaluation, but since we are not allowed to modify
+the classification_evaluation file, we have to create a new file.
+'''
+
+from torchvision import transforms
 from utils import *
 from model import * 
 from dataset import *
 from tqdm import tqdm
 from pprint import pprint
 import argparse
-import torch
-
+import csv
 NUM_CLASSES = len(my_bidict)
 
-#TODO: Begin of your code
-# classification function to convert the output of conditional PixelCNN++ to the prediction labels when given a new image
-def classify(model, model_input, device):
-    batch_size = model_input.shape[0]
+def get_label_and_losses(model, model_input, device):
+    _, labels, losses = model.infer_img(model_input, device)
+    return labels, losses
 
-    # replicate input for number of classes
-    model_input = model_input.repeat(NUM_CLASSES,1,1,1)
-
-    # lookup tensor of potential class labels that can be guessed for each image in the batch
-    batched_labels = torch.arange(NUM_CLASSES).repeat_interleave(batch_size)
-    
-    # generate output for each class label
-    model_out = model(model_input, batched_labels)
-
-    # choice of loss function was given from piazza/ TA office hours
-    logits = discretized_mix_logistic_loss(model_input, model_out, sum_over_batch=False).view(NUM_CLASSES, batch_size).permute(1, 0)
-    
-    # minimize logistic loss
-    losses, pred_labels = torch.min(logits, dim=1)
-
-    return logits, losses, pred_labels
-
-def get_label(model, model_input, device):
-    logits, losses, pred_labels = classify(model, model_input, device)
-    return pred_labels
-# End of your code
-
-def classifier(model, data_loader, device):
+# Modified to include dataset to write to csv and npy
+def classifier(model, data_loader, device, ds):
     model.eval()
-    acc_tracker = ratio_tracker()
-    # Lists to store predictions and image IDs
-    all_preds = []
-    all_image_ids = []
-    
-    for batch_idx, item in enumerate(tqdm(data_loader)):
-        model_input, categories = item
+    preds = []
+    logits = []
+    for _, item in enumerate(tqdm(data_loader)):
+        model_input, _ = item
         model_input = model_input.to(device)
-        original_label = [my_bidict[item] for item in categories if item in my_bidict]
-        original_label = torch.tensor(original_label, dtype=torch.int64).to(device)
-        answer = get_label(model, model_input, device)
-        correct_num = torch.sum(answer == original_label)
-        acc_tracker.update(correct_num.item(), model_input.shape[0])
-        
-        # Store predictions
-        all_preds.extend(answer.cpu().numpy().tolist())
-    
-    # Save to CSV
-    csv_path = os.path.join(os.path.dirname(__file__), 'classifier_results.csv')
-    with open(csv_path, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        # Write base filename and predicted label, no header
-        for image_path, pred in zip(dataset.samples, all_preds):
-            img_name = os.path.basename(image_path[0])
-            csv_writer.writerow([img_name, pred])
-    
-    print(f"Results saved to {csv_path}")
-    return acc_tracker.get_ratio()
+        pred, losses = get_label_and_losses(model, model_input, device)
+        preds.append(pred)
+        logits = np.append(logits, losses.cpu().detach().numpy())
+    preds = torch.cat(preds, -1)
+
+    # Reshape the logits array
+    logits = logits.reshape(-1, NUM_CLASSES)
+    print(logits.shape)
+    print(logits)
+    np.save('test_logits.npy', logits)
+
+    # write the final result csv file
+    with open("submission.csv", mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['id', 'label'])
+
+        for path, label in zip(ds.samples, preds):
+            name = os.path.basename(path[0])
+            writer.writerow([name, label.item()])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -85,26 +62,15 @@ if __name__ == '__main__':
     kwargs = {'num_workers':0, 'pin_memory':True, 'drop_last':False}
 
     ds_transforms = transforms.Compose([transforms.Resize((32, 32)), rescaling])
-    dataloader = torch.utils.data.DataLoader(CPEN455Dataset(root_dir=args.data_dir, 
-                                                            mode=args.mode, 
-                                                            transform=ds_transforms), 
+    ds = CPEN455Dataset(root_dir=args.data_dir, mode=args.mode, transform=ds_transforms)
+    dataloader = torch.utils.data.DataLoader(ds, 
                                              batch_size=args.batch_size, 
-                                             shuffle=True, 
+                                             shuffle=False, 
                                              **kwargs)
-    
-    dataset = CPEN455Dataset(root_dir=args.data_dir, mode=args.mode, transform=ds_transforms)
-    #TODO:Begin of your code
-    model = PixelCNN(nr_resnet=1, nr_filters=40, input_channels=3, nr_logistic_mix=5, num_classes=NUM_CLASSES)
-    #End of your code
 
+    model = PixelCNN(nr_resnet=1, nr_filters=40, input_channels=3, nr_logistic_mix=5, num_classes=NUM_CLASSES)
     model = model.to(device)
-    model_path = os.path.join(os.path.dirname(__file__), 'models/conditional_pixelcnn.pth')
-    if os.path.exists(model_path):
-        model.load_state_dict(torch.load(model_path))
-        print('model parameters loaded')
-    else:
-        raise FileNotFoundError(f"Model file not found at {model_path}")
+    model.load_state_dict(torch.load('models/conditional_pixelcnn.pth'))
     model.eval()
-    
-    acc = classifier(model=model, data_loader=dataloader, device=device)
-    print(f"Accuracy: {acc}")
+    print('model parameters loaded')
+    classifier(model = model, data_loader = dataloader, device = device, ds=ds)
