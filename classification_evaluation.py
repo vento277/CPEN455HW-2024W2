137960efdb624c971816afba68d95dba6c96e15a
vento277 <blacksoup0112@gmail.com>
2025-04-12 13:25:35 -0700

    update

--- classification_evaluation.py
+++ classification_evaluation.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
 
     #TODO:Begin of your code
     #You should replace the random classifier with your trained model
-    model = PixelCNN(nr_resnet=1, nr_filters=100, input_channels=3, nr_logistic_mix=10, num_classes=NUM_CLASSES)
+    model = PixelCNN(nr_resnet=2, nr_filters=100, input_channels=3, nr_logistic_mix=10, num_classes=NUM_CLASSES)
     #End of your code
 
     model = model.to(device)
