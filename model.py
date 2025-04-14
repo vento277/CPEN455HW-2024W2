46711eafc24f33f673a2453d29ee3ae196e39f38
vento277 <blacksoup0112@gmail.com>
2025-04-14 15:51:48 -0700

    update

--- model.py
+++ model.py
@@ -1,10 +1,5 @@
 import torch.nn as nn
-import torch.nn.functional as F
 from layers import *
-import torch
-from torch.autograd import Variable
-import os
-import pdb
 
 
 class PixelCNNLayer_up(nn.Module):
@@ -13,21 +8,21 @@ class PixelCNNLayer_up(nn.Module):
         self.nr_resnet = nr_resnet
         # stream from pixels above
         self.u_stream = nn.ModuleList([gated_resnet(nr_filters, down_shifted_conv2d,
-                                                     resnet_nonlinearity, skip_connection=0)
-                                         for _ in range(nr_resnet)])
+                                        resnet_nonlinearity, skip_connection=0)
+                                            for _ in range(nr_resnet)])
 
         # stream from pixels above and to thes left
         self.ul_stream = nn.ModuleList([gated_resnet(nr_filters, down_right_shifted_conv2d,
-                                                      resnet_nonlinearity, skip_connection=1)
-                                          for _ in range(nr_resnet)])
+                                        resnet_nonlinearity, skip_connection=1)
+                                            for _ in range(nr_resnet)])
 
     def forward(self, u, ul):
         u_list, ul_list = [], []
 
         for i in range(self.nr_resnet):
-            u = self.u_stream[i](u)
+            u  = self.u_stream[i](u)
             ul = self.ul_stream[i](ul, a=u)
-            u_list += [u]
+            u_list  += [u]
             ul_list += [ul]
 
         return u_list, ul_list
@@ -38,18 +33,18 @@ class PixelCNNLayer_down(nn.Module):
         super(PixelCNNLayer_down, self).__init__()
         self.nr_resnet = nr_resnet
         # stream from pixels above
-        self.u_stream = nn.ModuleList([gated_resnet(nr_filters, down_shifted_conv2d,
-                                                     resnet_nonlinearity, skip_connection=1)
-                                         for _ in range(nr_resnet)])
+        self.u_stream  = nn.ModuleList([gated_resnet(nr_filters, down_shifted_conv2d,
+                                        resnet_nonlinearity, skip_connection=1)
+                                            for _ in range(nr_resnet)])
 
         # stream from pixels above and to thes left
         self.ul_stream = nn.ModuleList([gated_resnet(nr_filters, down_right_shifted_conv2d,
-                                                      resnet_nonlinearity, skip_connection=2)
-                                          for _ in range(nr_resnet)])
+                                        resnet_nonlinearity, skip_connection=2)
+                                            for _ in range(nr_resnet)])
 
     def forward(self, u, ul, u_list, ul_list):
         for i in range(self.nr_resnet):
-            u = self.u_stream[i](u, a=u_list.pop())
+            u  = self.u_stream[i](u, a=u_list.pop())
             ul = self.ul_stream[i](ul, a=torch.cat((u, ul_list.pop()), 1))
 
         return u, ul
@@ -57,11 +52,11 @@ class PixelCNNLayer_down(nn.Module):
 
 class PixelCNN(nn.Module):
     def __init__(self, nr_resnet=5, nr_filters=80, nr_logistic_mix=10,
-                 resnet_nonlinearity='concat_elu', input_channels=3, num_classes=4):
+                    resnet_nonlinearity='concat_elu', input_channels=3, num_classes=4):
         super(PixelCNN, self).__init__()
-        if resnet_nonlinearity == 'concat_elu':
-            self.resnet_nonlinearity = lambda x: concat_elu(x)
-        else:
+        if resnet_nonlinearity == 'concat_elu' :
+            self.resnet_nonlinearity = lambda x : concat_elu(x)
+        else :
             raise Exception('right now only concat elu is supported as resnet nonlinearity.')
 
         self.num_classes = num_classes
@@ -69,34 +64,34 @@ class PixelCNN(nn.Module):
         self.input_channels = input_channels
         self.nr_logistic_mix = nr_logistic_mix
         self.right_shift_pad = nn.ZeroPad2d((1, 0, 0, 0))
-        self.down_shift_pad = nn.ZeroPad2d((0, 0, 1, 0))
+        self.down_shift_pad  = nn.ZeroPad2d((0, 0, 1, 0))
 
         down_nr_resnet = [nr_resnet] + [nr_resnet + 1] * 2
         self.down_layers = nn.ModuleList([PixelCNNLayer_down(down_nr_resnet[i], nr_filters,
-                                                             self.resnet_nonlinearity) for i in range(3)])
+                                                self.resnet_nonlinearity) for i in range(3)])
 
-        self.up_layers = nn.ModuleList([PixelCNNLayer_up(nr_resnet, nr_filters,
-                                                           self.resnet_nonlinearity) for _ in range(3)])
+        self.up_layers   = nn.ModuleList([PixelCNNLayer_up(nr_resnet, nr_filters,
+                                                self.resnet_nonlinearity) for _ in range(3)])
 
-        self.downsize_u_stream = nn.ModuleList([down_shifted_conv2d(nr_filters, nr_filters,
-                                                                    stride=(2, 2)) for _ in range(2)])
+        self.downsize_u_stream  = nn.ModuleList([down_shifted_conv2d(nr_filters, nr_filters,
+                                                    stride=(2,2)) for _ in range(2)])
 
         self.downsize_ul_stream = nn.ModuleList([down_right_shifted_conv2d(nr_filters,
-                                                                         nr_filters, stride=(2, 2)) for _ in range(2)])
+                                                    nr_filters, stride=(2,2)) for _ in range(2)])
 
-        self.upsize_u_stream = nn.ModuleList([down_shifted_deconv2d(nr_filters, nr_filters,
-                                                                    stride=(2, 2)) for _ in range(2)])
+        self.upsize_u_stream  = nn.ModuleList([down_shifted_deconv2d(nr_filters, nr_filters,
+                                                    stride=(2,2)) for _ in range(2)])
 
         self.upsize_ul_stream = nn.ModuleList([down_right_shifted_deconv2d(nr_filters,
-                                                                         nr_filters, stride=(2, 2)) for _ in range(2)])
+                                                    nr_filters, stride=(2,2)) for _ in range(2)])
 
-        self.u_init = down_shifted_conv2d(input_channels + 1, nr_filters, filter_size=(2, 3),
-                                          shift_output_down=True)
+        self.u_init = down_shifted_conv2d(input_channels + 1, nr_filters, filter_size=(2,3),
+                        shift_output_down=True)
 
         self.ul_init = nn.ModuleList([down_shifted_conv2d(input_channels + 1, nr_filters,
-                                                           filter_size=(1, 3), shift_output_down=True),
-                                      down_right_shifted_conv2d(input_channels + 1, nr_filters,
-                                                                filter_size=(2, 1), shift_output_right=True)])
+                                            filter_size=(1,3), shift_output_down=True),
+                                       down_right_shifted_conv2d(input_channels + 1, nr_filters,
+                                            filter_size=(2,1), shift_output_right=True)])
 
         num_mix = 3 if self.input_channels == 1 else 10
         self.nin_out = nin(nr_filters, num_mix * nr_logistic_mix)
@@ -104,10 +99,6 @@ class PixelCNN(nn.Module):
 
         # Add the embedding layer
         self.embedding = nn.Embedding(num_classes, input_channels * 32 * 32)
-
-        # Add a final 1x1 convolution layer before outputting
-        # This can help in better mixing the feature maps before the final prediction.
-        self.final_conv = nin(nr_filters, nr_filters)
 
     # Add the embeddings to the input
     def addPositionalEmbedding(self, x, labels, img_height, img_width):
@@ -125,29 +116,29 @@ class PixelCNN(nn.Module):
             padding = Variable(torch.ones(xs[0], 1, xs[2], xs[3]), requires_grad=False)
             self.init_padding = padding.cuda() if x.is_cuda else padding
 
-        if sample:
+        if sample :
             xs = [int(y) for y in x.size()]
             padding = Variable(torch.ones(xs[0], 1, xs[2], xs[3]), requires_grad=False)
             padding = padding.cuda() if x.is_cuda else padding
             x = torch.cat((x, padding), 1)
 
-        ###     UP PASS     ###
+        ###      UP PASS    ###
         x = x if sample else torch.cat((x, self.init_padding), 1)
-        u_list = [self.u_init(x)]
+        u_list  = [self.u_init(x)]
         ul_list = [self.ul_init[0](x) + self.ul_init[1](x)]
         for i in range(3):
             # resnet block
             u_out, ul_out = self.up_layers[i](u_list[-1], ul_list[-1])
-            u_list += u_out
+            u_list  += u_out
             ul_list += ul_out
 
             if i != 2:
                 # downscale (only twice)
-                u_list += [self.downsize_u_stream[i](u_list[-1])]
+                u_list  += [self.downsize_u_stream[i](u_list[-1])]
                 ul_list += [self.downsize_ul_stream[i](ul_list[-1])]
 
         ###    DOWN PASS    ###
-        u = u_list.pop()
+        u  = u_list.pop()
         ul = ul_list.pop()
 
         for i in range(3):
@@ -155,19 +146,16 @@ class PixelCNN(nn.Module):
             u, ul = self.down_layers[i](u, ul, u_list, ul_list)
 
             # upscale (only twice)
-            if i != 2:
-                u = self.upsize_u_stream[i](u)
+            if i != 2 :
+                u  = self.upsize_u_stream[i](u)
                 ul = self.upsize_ul_stream[i](ul)
 
-        # Apply the final 1x1 convolution
-        ul = F.elu(ul)
-        ul = self.final_conv(ul)
-        x_out = self.nin_out(ul)
+        x_out = self.nin_out(F.elu(ul))
 
         assert len(u_list) == len(ul_list) == 0, pdb.set_trace()
 
         return x_out
-
+    
     # Run model inference
     def infer_img(self, x, device):
         B, _, _, _ = x.size()
@@ -183,8 +171,7 @@ class PixelCNN(nn.Module):
         # Get the minimum loss and the corresponding label
         losses, labels = torch.min(inferred_loss, dim=0)
         return losses, labels, inferred_loss
-
-
+    
 class random_classifier(nn.Module):
     def __init__(self, NUM_CLASSES):
         super(random_classifier, self).__init__()
@@ -195,6 +182,7 @@ class random_classifier(nn.Module):
         if 'models' not in os.listdir():
             os.mkdir('models')
         torch.save(self.state_dict(), 'models/conditional_pixelcnn.pth')
-
     def forward(self, x, device):
         return torch.randint(0, self.NUM_CLASSES, (x.shape[0],)).to(device)
+    
+    
--- pcnn_train.py
+++ pcnn_train.py
@@ -11,6 +11,7 @@ from dataset import *
 from tqdm import tqdm
 from pprint import pprint
 import argparse
+from classification_evaluation import *
 from pytorch_fid.fid_score import calculate_fid_given_paths
 
 
@@ -23,6 +24,11 @@ def train_or_test(model, data_loader, optimizer, loss_op, device, args, epoch, m
     deno =  args.batch_size * np.prod(args.obs) * np.log(2.)        
     loss_tracker = mean_tracker()
     val_acc_tracker = mean_tracker()
+
+    my_bidict = bidict({'Class0': 0, 
+                'Class1': 1,
+                'Class2': 2,
+                'Class3': 3})
     
     for _, item in enumerate(tqdm(data_loader)):
         model_input, labels = item
@@ -30,7 +36,7 @@ def train_or_test(model, data_loader, optimizer, loss_op, device, args, epoch, m
 
         # Check if the model is in training mode or test mode
         if mode == 'test':
-            losses, label_preds, _ = model.infer_img(model_input, device)
+            losses, label_preds = model.infer_img(model_input, device)
             loss_tracker.update(torch.sum(losses).item()/deno)
         else:
             labels = torch.tensor([my_bidict[item] for item in labels])
@@ -51,8 +57,7 @@ def train_or_test(model, data_loader, optimizer, loss_op, device, args, epoch, m
     if args.en_wandb:
         wandb.log({mode + "-Average-BPD" : loss_tracker.get_mean()})
         wandb.log({mode + "-epoch": epoch})
-        if mode == 'val':
-            wandb.log({"val-Accuracy": val_acc_tracker.get_mean()})
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -82,11 +87,11 @@ if __name__ == '__main__':
                         help='Observation shape')
     
     # model
-    parser.add_argument('-q', '--nr_resnet', type=int, default=5,
+    parser.add_argument('-q', '--nr_resnet', type=int, default=1,
                         help='Number of residual blocks per stage of the model')
-    parser.add_argument('-n', '--nr_filters', type=int, default=160,
+    parser.add_argument('-n', '--nr_filters', type=int, default=40,
                         help='Number of filters to use across the model. Higher = larger model.')
-    parser.add_argument('-m', '--nr_logistic_mix', type=int, default=10,
+    parser.add_argument('-m', '--nr_logistic_mix', type=int, default=5,
                         help='Number of logistic components in the mixture. Higher = more flexible model')
     parser.add_argument('-l', '--lr', type=float,
                         default=0.0002, help='Base learning rate')
@@ -135,8 +140,10 @@ if __name__ == '__main__':
 
     #set device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    kwargs = {'num_workers':0, 'pin_memory':True, 'drop_last':True}
-    print('Using device:', device)
+    #Reminder: if you have patience to read code line by line, you should notice this comment. here is the reason why we set num_workers to 0:
+    #In order to avoid pickling errors with the dataset on different machines, we set num_workers to 0.
+    #If you are using ubuntu/linux/colab, and find that loading data is too slow, you can set num_workers to 1 or even bigger.
+    kwargs = {'num_workers':1, 'pin_memory':True, 'drop_last':True}
 
     # set data
     if "mnist" in args.dataset:
@@ -237,10 +244,27 @@ if __name__ == '__main__':
                       epoch = epoch,
                       mode = 'val')
         
+        val_accuracy = classifier(model, val_loader, device)
+        if args.en_wandb:
+          wandb.log({"Validation Accuracy": val_accuracy, "epoch": epoch + 1})
+
         if epoch % args.sampling_interval == 0:
             print('......sampling......')
-            labels = torch.randint(0, num_classes, (args.sample_batch_size,)).to(next(model.parameters()).device)
-            sample_t = sample(model, args.sample_batch_size, args.obs, sample_op, labels=labels)
+            # generate random labels to feed into samples
+            # rand_labels = torch.randint(low=0, high=len(my_bidict), size=(args.sample_batch_size,)).to(device=next(model.parameters()).device)
+            
+            # generate ordered labels for each class section
+            section_size = args.sample_batch_size // 4
+            ordered_labels = torch.cat([
+                torch.full((section_size,), 0, dtype=torch.long), 
+                torch.full((section_size,), 1, dtype=torch.long), 
+                torch.full((section_size,), 2, dtype=torch.long),  
+                torch.full((section_size,), 3, dtype=torch.long)   
+            ])
+
+            # added labels tensor as param
+            sample_t = sample(model, args.sample_batch_size, args.obs, sample_op, ordered_labels)
+            
             sample_t = rescaling_inv(sample_t)
             save_images(sample_t, args.sample_dir)
             sample_result = wandb.Image(sample_t, caption="epoch {}".format(epoch))
@@ -257,9 +281,9 @@ if __name__ == '__main__':
             if args.en_wandb:
                 wandb.log({"samples": sample_result,
                             "FID": fid_score})
-        
+
         if (epoch + 1) % args.save_interval == 0: 
             if not os.path.exists("models"):
                 os.makedirs("models")
-            # torch.save(model.state_dict(), 'models/{}_{}.pth'.format(model_name, epoch))
             torch.save(model.state_dict(), 'models/conditional_pixelcnn.pth'.format(model_name, epoch))
+            # torch.save(model.state_dict(), 'models/{}_{}.pth'.format(model_name, epoch))
