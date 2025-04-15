c60a6979415ac04fbf9e5bb1a9e761adfcf68c51
vento277 <blacksoup0112@gmail.com>
2025-04-15 00:40:57 -0700

    update

--- generation_evaluation.py
+++ generation_evaluation.py
@@ -15,14 +15,13 @@ import torch
 # You should modify this sample function to get the generated images from your model
 # This function should save the generated images to the gen_data_dir, which is fixed as 'samples'
 # You should save the generated images to the gen_data_dir, which is fixed as 'samples'
-# Begin of your code
 sample_op = lambda x : sample_from_discretized_mix_logistic(x, 5)
 def my_sample(model, gen_data_dir, sample_batch_size = 25, obs = (3,32,32), sample_op = sample_op):
     for label in my_bidict:
         print(f"Label: {label}")
         #generate images for each label, each label has 25 images
-        labels = torch.ones(sample_batch_size, dtype=torch.int64).to(next(model.parameters()).device) * my_bidict[label]
-        sample_t = sample(model, sample_batch_size, obs, sample_op, labels)
+        label_tensor = torch.full((sample_batch_size,), my_bidict[label], dtype=torch.long, device=device)  #same with classification, create a label tensor to feed into sample
+        sample_t = sample(model, sample_batch_size, obs, sample_op, class_label=label_tensor)
         sample_t = rescaling_inv(sample_t)
         save_images(sample_t, os.path.join(gen_data_dir), label=label)
     pass
@@ -39,8 +38,8 @@ if __name__ == "__main__":
     #Begin of your code
     #Load your model and generate images in the gen_data_dir
     model = PixelCNN(nr_resnet=1, nr_filters=50, input_channels=3, nr_logistic_mix=5)
-    model.load_state_dict(torch.load('models/conditional_pixelcnn.pth'))
     model = model.to(device)
+    model.load_state_dict(torch.load('models/conditional_pixelcnn.pth'))
     model = model.eval()
     #End of your code
 
