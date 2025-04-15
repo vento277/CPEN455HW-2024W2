19383a9b9e9c2d8e61f136c740d6a83fd2a8e692
vento277 <blacksoup0112@gmail.com>
2025-04-14 17:06:19 -0700

    update

--- classification_evaluation.py
+++ classification_evaluation.py
@@ -2,13 +2,9 @@
 This code is used to evaluate the classification accuracy of the trained model.
 You should at least guarantee this code can run without any error on validation set.
 And whether this code can run is the most important factor for grading.
-We provide the remaining code, all you should do are, and you can't modify other code:
-1. Replace the random classifier with your trained model.(line 69-72)
-2. modify the get_label function to get the predicted label.(line 23-29)(just like Leetcode solutions, the args of the function can't be changed)
-
-REQUIREMENTS:
-- You should save your model to the path 'models/conditional_pixelcnn.pth'
-- You should Print the accuracy of the model on validation set, when we evaluate your code, we will use test set to evaluate the accuracy
+We provide the remaining code, all you should do are, and you can't modify the remaining code:
+1. Replace the random classifier with your trained model.(line 64-68)
+2. modify the get_label function to get the predicted label.(line 18-24)(just like Leetcode solutions)
 '''
 from torchvision import datasets, transforms
 from utils import *
@@ -17,38 +13,14 @@ from dataset import *
 from tqdm import tqdm
 from pprint import pprint
 import argparse
-import csv
 NUM_CLASSES = len(my_bidict)
 
-#TODO: Begin of your code
-# classification function to convert the output of conditional PixelCNN++ to the prediction labels when given a new image
-def classify(model, model_input, device):
-    batch_size = model_input.shape[0]
-
-    # replicate input for number of classes
-    model_input = model_input.repeat(NUM_CLASSES,1,1,1)
-
-    # lookup tensor of potential class labels that can be guessed for each image in the batch
-    # class label is repeated to match a batch of data; for parallel processing
-    # eg.) when batch_size=3, num_classes=4
-    # shape: ([0, 0, 0, 1, 1, 1, 2, 2, 2])
-    batched_labels = torch.arange(NUM_CLASSES).repeat_interleave(batch_size)
-    
-    # generate output for each class label
-    model_out = model(model_input, batched_labels)
-
-    # choice of loss function was given from piazza/ TA office hours
-    logits = discretized_mix_logistic_loss(model_input, model_out, sum_over_batch=False).view(NUM_CLASSES, batch_size).permute(1, 0)
-    
-    # minimize logistic loss
-    losses, pred_labels = torch.min(logits, dim=1)
-
-    return logits, losses, pred_labels
-
+# Write your code here
+# And get the predicted label, which is a tensor of shape (batch_size,)
+# Begin of your code
 def get_label(model, model_input, device):
-    # changed to predicted
-    logits, losses, pred_labels = classify(model, model_input, device)
-    return pred_labels
+    _, labels = model.infer_img(model_input, device)
+    return labels
 # End of your code
 
 def classifier(model, data_loader, device):
@@ -89,11 +61,12 @@ if __name__ == '__main__':
                                              shuffle=True, 
                                              **kwargs)
 
-    #TODO:Begin of your code
+    #Write your code here
     #You should replace the random classifier with your trained model
+    #Begin of your code
     model = PixelCNN(nr_resnet=1, nr_filters=50, input_channels=3, nr_logistic_mix=5, num_classes=NUM_CLASSES)
     #End of your code
-
+    
     model = model.to(device)
     #Attention: the path of the model is fixed to './models/conditional_pixelcnn.pth'
     #You should save your model to this path
